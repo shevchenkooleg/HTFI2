b886bcce129c2a3a1739ca34bcc9943f6ae764ce
Oleg <shevchenko.oleg.87@gmail.com>
2024-03-17 15:43:15 +0300

    hw13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -74,7 +74,7 @@ const HW13 = () => {
     return (
         <div id={'hw13'}>
             <div className={s2.hwTitle}>Homework #13</div>
-
+            <hr/>
             <div className={s2.hw}>
                 <div className={s.buttonsContainer}>
                     <SuperButton
